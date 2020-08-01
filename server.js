ee0c57420b4ba1745a1ecf0aa53d2a2493fc9388
sbgarcia <sharonbondoc.g@gmail.com>
2020-08-01 15:19:54 -0700

    created new route and rendered superhero index page view with updated data

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const mongoose = require ('mongoose');
 
 const Superhero = require('./models/superhero.js');
-const { response } = require('express');
+
 
 // Middleware
 
@@ -13,6 +13,7 @@ app.use(express.urlencoded({extended:false}));
 
 // Database connection
 const connectionString = 'mongodb://localhost/superheros'
+
 mongoose.connect(connectionString, {
   useNewUrlParser: true,
   useUnifiedTopology: true, 
@@ -33,6 +34,28 @@ mongoose.connection.on('error', (err) => {
   console.log('mongoose error: ', err)
 });
 
+// New Super Hero Route
+
+app.get('/superheros/new', (request, response) => {
+  response.render ('superheros/new.ejs')
+});
+
+// Create Superhero Route
+app.post('/superheros', (request, response) => {
+  Superhero.create(request.body, (err, createdSuperhero) => {
+    response.redirect('/superheros/gallery')
+    });
+  });
+
+// Index Route for Super Heros
+
+app.get('/superheros/gallery', (request, response) => {
+  Superhero.find({}, (err, allSuperheros) => {
+    response.render('superheros/index.ejs', {
+      superheros: allSuperheros
+    });  
+  });
+});
 
 // Homepage Index Route
 
@@ -40,40 +63,26 @@ app.get('/superheros',(request, response) => {
   response.render('home.ejs')
 });
 
-// Index Route for Super Heros
+// // Update Route
 
-app.get('/superheros/gallery', (request, response) => {
-  response.render('superheros/index.ejs')
-});
+// app.get('/superheros/show', (request, response) => {
+//   response.render ('superheros/show.ejs')
+// });
 
-// Create a New Super Hero Route
 
-app.get('/superheros/new', (request, response) => {
-  response.render ('superheros/new.ejs')
-});
+// // Show Route
+// app.get('/superheros/gallery', (request, response) => {
+//   Superhero.find({}, (err, allSuperheros) => {
+//     response.render('/superheros/index.ejs', {
+//       superheros: allSuperheros
+//     })
+//   })
+// })
 
-// Update Route
 
-app.get('/superheros/show', (request, response) => {
-  response.render ('superheros/show.ejs')
-});
-
-// Update Route
-app.post('/superheros/', (request, response) => {
-  Superhero.create(request.body, (err, createdSuperhero) => {
-    response.redirect('/superheros')
-  })
-})
-
-// Show Route
-app.get('/superheros', (request, response) => {
-  Superhero.find({}, (err, foundSuperheros) => {
-    response.render('/superheros/index.ejs', {
-      superheros: foundSuperheros
-    })
-  })
-})
-
+//delete
+//edit
+//update
 
 
 //  Listen function
