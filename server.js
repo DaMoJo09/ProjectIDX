83dff183d64f646c47fd75e21908c7cc18097b53
sbgarcia <sharonbondoc.g@gmail.com>
2020-08-01 13:41:52 -0700

    created superhero model

--- server.js
+++ server.js
@@ -1,18 +1,23 @@
-const mongoose = require ('mongoose');
-
+// Require statements
 const express = require('express');
-const { response } = require('express');
-const { request } = require('http');
 const app = express();
 
+const mongoose = require ('mongoose');
 
+const Superhero = require('./models/superhero.js');
+
+// Middleware
+
+app.use(express.urlencoded({extended:false}));
+
+// Database connection
 const connectionString = 'mongodb://localhost/superheros'
 mongoose.connect(connectionString, {
   useNewUrlParser: true,
   useUnifiedTopology: true, 
   useCreateIndex: true, 
   useFindAndModify: false
-})
+});
 
 mongoose.connection.on('connected', () => {
   console.log(`mongoose connected to ${connectionString}`)
@@ -21,38 +26,41 @@ mongoose.connection.on('connected', () => {
 
 mongoose.connection.on('disconnected', () =>{
   console.log('mongoose disconnected')
-})
+});
 
-// mongoose.connection.on('err', )
+mongoose.connection.on('error', (err) => {
+  console.log('mongoose error: ', err)
+});
 
-//  Listen function
-
-app.listen(3000, () => {
-  console.log('Heros GO!')
-})
 
 // Index Route
 
 app.get('/superheros',(request, response) => {
   response.render('index.ejs')
-})
+});
 
 // Index Route for Super Heros
 
 app.get('/superheros/gallery', (request, response) => {
   response.render('superheros/index.ejs')
-})
+});
 
 // Create a New Super Hero Route
 
 app.get('/superheros/new', (request, response) => {
   response.render ('superheros/new.ejs')
-})
+});
 
 // Update Route
 
 app.get('/superheros/show', (request, response) => {
   response.render ('superheros/show.ejs')
-})
+});
 
 
+
+//  Listen function
+
+app.listen(3000, () => {
+  console.log('Heros GO!')
+});
