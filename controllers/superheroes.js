ea9e5e587e69694d00d6863fb9727f334f57ed85
yourehilarryous <yourehilarryous@gmail.com>
2020-08-02 15:08:23 -0700

    Refactored routes into controller files

---
+++ controllers/superheroes.js
@@ -0,0 +1,68 @@
+const express = require("express")
+const Superhero = require('../models/superhero.js');
+const Powers = require("../models/powers.js")
+const router = express.Router()
+
+// New Superhero Route
+router.get('/new', (request, response) => {
+    response.render ('superheroes/new.ejs')
+  });
+  
+  // Create Superhero Route
+  router.post('/', (request, response) => {
+    Superhero.create(request.body, (err, createdSuperhero) => {
+      response.redirect('/superheroes/gallery')
+      });
+    });
+  
+  // Index Route for Superheroes
+  router.get('/gallery', (request, response) => {
+    Superhero.find({}, (err, allSuperheroes) => {
+      response.render('superheroes/index.ejs', {
+        superheroes: allSuperheroes
+      });  
+    });
+  });
+  
+  // Homepage Index Route
+  router.get('/',(request, response) => {
+    response.render('home.ejs')
+  });
+  
+  
+  // Show Route for Superheroes
+  router.get('/:id', (request, response) => {
+      Superhero.findById(request.params.id, (err, foundSuperhero) => {
+          response.render('superheroes/show.ejs', {
+              superheroes: foundSuperhero
+      });
+    });
+  });
+        
+        
+  // Delete Route for Superheroes
+  router.delete('/:id', (request, response) => {
+    Superhero.findByIdAndDelete(request.params.id, () => {
+      response.redirect('/superheroes/gallery')
+    });
+  });
+  
+  
+  // Edit Route for Superheroes
+  router.get('/:id/edit', (request, response) => {
+    Superhero.findById(request.params.id, (err, foundSuperhero) => {
+      response.render('superheroes/edit.ejs', {
+        superheroes: foundSuperhero
+      });
+    });
+  });
+  
+  // Update Route for Superheroes
+  router.put('/:id', (request, response) => {
+    Superhero.findByIdAndUpdate(request.params.id, request.body, () => {
+      response.redirect('/superheroes/gallery')
+    });
+  });
+
+  
+  module.exports = router
--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const methodOverride = require('method-override');
 
 // Models
 const Superhero = require('./models/superhero.js');
-const Powers = require("../models/powers.js")
+const Powers = require("./models/powers.js")
 
 // Middleware
 app.use(methodOverride('_method'));
@@ -38,68 +38,10 @@ mongoose.connection.on('error', (err) => {
   console.log('mongoose error: ', err)
 });
 
-// New Superhero Route
-app.get('/superheroes/new', (request, response) => {
-  response.render ('superheroes/new.ejs')
-});
-
-// Create Superhero Route
-app.post('/superheroes', (request, response) => {
-  Superhero.create(request.body, (err, createdSuperhero) => {
-    response.redirect('/superheroes/gallery')
-    });
-  });
-
-// Index Route for Superheroes
-app.get('/superheroes/gallery', (request, response) => {
-  Superhero.find({}, (err, allSuperheroes) => {
-    response.render('superheroes/index.ejs', {
-      superheroes: allSuperheroes
-    });  
-  });
-});
-
-// Homepage Index Route
-app.get('/superheroes',(request, response) => {
-  response.render('home.ejs')
-});
-
-
-// Show Route for Superheroes
-app.get('/superheroes/:id', (request, response) => {
-    Superhero.findById(request.params.id, (err, foundSuperhero) => {
-        response.render('superheroes/show.ejs', {
-            superheroes: foundSuperhero
-    });
-  });
-});
-      
-      
-// Delete Route for Superheroes
-app.delete('/superheroes/:id', (request, response) => {
-  Superhero.findByIdAndDelete(request.params.id, () => {
-    response.redirect('/superheroes/gallery')
-  });
-});
-
-
-// Edit Route for Superheroes
-app.get('/superheroes/:id/edit', (request, response) => {
-  Superhero.findById(request.params.id, (err, foundSuperhero) => {
-    response.render('superheroes/edit.ejs', {
-      superheroes: foundSuperhero
-    });
-  });
-});
-
-// Update Route for Superheroes
-app.put('/superheroes/:id', (request, response) => {
-  Superhero.findByIdAndUpdate(request.params.id, request.body, () => {
-    response.redirect('/superheroes/gallery')
-  });
-});
-
-
+const heroRouter = require("./controllers/superheroes")
+const powerRouter = require("./controllers/powers")
+app.use("/superheroes", heroRouter)
+app.use("/superheroes", powerRouter)
       
 //  Listen function
 app.listen(3000, () => {
