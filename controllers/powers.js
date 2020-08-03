25479bbc8909e2bddd5dc35c7f77c287276798bc
sbgarcia <sharonbondoc.g@gmail.com>
2020-08-02 23:57:16 -0700

    created new and create routes for powers and new.ejs

--- controllers/powers.js
+++ controllers/powers.js
@@ -5,6 +5,17 @@ const Power = require('../models/power');
 
 const router = express.Router();
 
+// New Power Route
+router.get('/new', (request, response) => {
+    response.render ('powers/new.ejs')
+});
+  
+// Create Power Route
+router.post('/', (request, response) => {
+    Power.create(request.body, (err, createdPower) => {
+        response.redirect('/powers/index')
+    });
+});
 
 // Index Route for Superhero Powers
 router.get('/index', (request, response) => {
